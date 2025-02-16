e416f0022703efb5d56637b7821dc0daac820a82
Enzo Mendez <enzo.mendez315@gmail.com>
2025-02-15 20:05:50 -0700

    Pushing Spring Boot setup.

---
+++ backend/src/main/java/com/enzomendez/preppit/PreppitApplication.java
@@ -0,0 +1,13 @@
+package com.enzomendez.preppit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreppitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreppitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/enzomendez/preppit/PreppitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzomendez.preppit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreppitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

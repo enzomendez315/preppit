3b8e6cd0407bb6c48bec3c00da9b25aaa398070b
Enzo Mendez <enzo.mendez315@gmail.com>
2025-02-16 16:46:19 -0700

    Added getters and setter for User model.

---
+++ backend/src/main/java/com/enzomendez/preppit/model/Item.java
@@ -0,0 +1,6 @@
+package com.enzomendez.preppit.model;
+
+public class Item {
+    private Long id;
+    private String name;
+}
---
+++ backend/src/main/java/com/enzomendez/preppit/model/User.java
@@ -0,0 +1,61 @@
+package com.enzomendez.preppit.model;
+
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {}
+
+    public User(String firstName,
+                String lastName,
+                String username,
+                String email,
+                String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}

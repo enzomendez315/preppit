1194c4c96611c0db1638c602d0242e7f1fa6296d
Enzo Mendez <enzo.mendez315@gmail.com>
2025-03-04 00:07:15 -0700

    Implement JwtAuthenticationFilter for authentication.

--- backend/src/main/java/com/enzomendez/preppit/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/enzomendez/preppit/config/JwtAuthenticationFilter.java
@@ -6,6 +6,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,10 +21,12 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Autowired
-    public JwtAuthenticationFilter(JwtService jwtService) {
+    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
         this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
     }
 
     @Override
@@ -28,7 +36,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authorizationHeader = request.getHeader("Authorization");
         final String jwtToken;
-        final String email;
+        final String user;
 
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
@@ -36,6 +44,24 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         jwtToken = authorizationHeader.substring(7);
-        email = jwtService.extractUser(jwtToken);
+        user = jwtService.extractUser(jwtToken);
+
+        // User is not authenticated yet
+        if (user != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(user);
+
+            if (jwtService.isValidToken(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                // Update security context holder
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        // Execute the next filter in the chain
+        filterChain.doFilter(request, response);
     }
 }
--- backend/src/main/java/com/enzomendez/preppit/user/UserRepository.java
+++ backend/src/main/java/com/enzomendez/preppit/user/UserRepository.java
@@ -3,10 +3,10 @@ package com.enzomendez.preppit.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public Optional<User> findByEmail(String email) {
-
-    }
+    public Optional<User> findByEmail(String email);
 }

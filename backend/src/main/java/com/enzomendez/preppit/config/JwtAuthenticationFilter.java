b93cfba2c6a47e0aae36c436309f9a3ef064f2ba
Enzo Mendez <enzo.mendez315@gmail.com>
2025-03-03 20:21:17 -0700

    Implement JwtService.

---
+++ backend/src/main/java/com/enzomendez/preppit/config/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.enzomendez.preppit.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Autowired
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        final String email;
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwtToken = authorizationHeader.substring(7);
+        email = jwtService.extractUser(jwtToken);
+    }
+}
--- backend/src/main/java/com/enzomendez/preppit/user/UserRepository.java
+++ backend/src/main/java/com/enzomendez/preppit/user/UserRepository.java
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public Optional<User> findByEmail(String email) {
+
+    }
 }
